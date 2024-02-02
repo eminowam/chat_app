c2db6ca07e1a4ea5331747bcc750c681684a1d6c
aimeerimeminova <aimemynoo@gmail.com>
2024-02-02 16:38:25 +0600

    add firebase

--- lib/my_app.dart
+++ lib/my_app.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:mozz/chat_screen.dart';
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -7,7 +8,11 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-
+      title : 'Chat App' ,
+      theme : ThemeData (
+        primarySwatch : Colors.blue,
+      ),
+      home : ChatScreen (),
     );
   }
 }
